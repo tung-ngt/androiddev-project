25ae26f826f38807fbfb1b3c293c483127ae4bae
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2023-09-15 21:39:21 +0700

    Make three new fragment layout xml

---
+++ app/src/main/java/com/tungngt/dev/ui/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.tungngt.dev.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tungngt.dev.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_register);
+    }
+}
