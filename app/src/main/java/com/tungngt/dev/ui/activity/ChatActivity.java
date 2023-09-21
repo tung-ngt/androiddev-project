2942937438e530bb730bc4f48b4341ec4cb83672
MTuchiha <minhtung290403@gmail.com>
2023-09-21 21:16:40 +0700

    Add recyclerview

--- app/src/main/java/com/tungngt/dev/ui/activity/ChatActivity.java
+++ app/src/main/java/com/tungngt/dev/ui/activity/ChatActivity.java
@@ -24,18 +24,34 @@ import android.widget.Button;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.tungngt.dev.R;
 import com.tungngt.dev.databinding.ChatLayoutBinding;
+
+import com.tungngt.dev.model.Message;
+import com.tungngt.dev.ui.adapter.ChatAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChatActivity extends AppCompatActivity{
-    private static final String TAG = "ChatActivity";
+    private ChatLayoutBinding chatLayoutBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.chat_layout);
-        Log.d(TAG, "onCreate: started");
+        chatLayoutBinding = ChatLayoutBinding.inflate(getLayoutInflater());
+        setContentView(chatLayoutBinding.getRoot());
+        List<Message> messages = new ArrayList<>();
+        messages.add(new Message("Tung", "hello", "12:00", "1"));
+        messages.add(new Message("Tung", "how are you", "12:00", "1"));
+        messages.add(new Message("Tung", "hi", "12:00", "1"));
+        messages.add(new Message("Tung", "Feeder XD", "12:00", "1"));
+        ChatAdapter chatAdapter = new ChatAdapter();
+        chatAdapter.differ.submitList(messages);
+        chatLayoutBinding.chatRecyclerView.setAdapter(chatAdapter);
+
         MaterialToolbar navIcon = findViewById(R.id.topAppBar);
         navIcon.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d(TAG, "onClick: clicked navIcon.");
                 Intent intent = new Intent(ChatActivity.this, MainActivity.class);
                 startActivity(intent);
             }
