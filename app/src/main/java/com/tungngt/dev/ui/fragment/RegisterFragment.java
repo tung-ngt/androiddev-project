681b14f28fc45e920fae2246bce66700efbaa51d
Nguyen Thanh Tung <tung.ngt@outlook.com>
2023-09-22 11:35:50 +0700

    merge authentication activity

--- app/src/main/java/com/tungngt/dev/ui/fragment/RegisterFragment.java
+++ app/src/main/java/com/tungngt/dev/ui/fragment/RegisterFragment.java
@@ -5,26 +5,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.tungngt.dev.R;
 import com.tungngt.dev.databinding.FragmentRegisterBinding;
+
+import java.util.Objects;
 
 
 public class RegisterFragment extends Fragment{
     private FragmentRegisterBinding fragmentRegisterBinding;
     @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return fragmentRegisterBinding.getRoot();
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         fragmentRegisterBinding = FragmentRegisterBinding.inflate(getLayoutInflater());
-    }
-    public RegisterFragment(){
 
+        fragmentRegisterBinding.signupButton.setOnClickListener(this::signup);
     }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_register, container, false);
+
+    private void signup(View view) {
+        NavController navController = NavHostFragment.findNavController(
+                Objects.requireNonNull(
+                        requireActivity()
+                                .getSupportFragmentManager()
+                                .findFragmentById(R.id.nav_host_fragment_container)
+                )
+        );
+        navController.popBackStack();
     }
 }
